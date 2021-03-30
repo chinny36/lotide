20753f32cc7ce1d2ba1cdfd51e5adeed7f637570
James Brown <brownj95@gmail.com>
2021-03-30 21:07:38 +0000

    Implemented eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}✅`);
+  } else {
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}🛑`);
+  }
+
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  const isEqual = (currentElement, currentIndex) => currentElement === arrayTwo[currentIndex];
+  return arrayOne.every(isEqual);
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
