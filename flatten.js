e5ae48e2e504a845afa35e48942a2893b2c12000
James Brown <brownj95@gmail.com>
2021-03-30 23:52:56 +0000

    Implemented flatten.js

---
+++ flatten.js
@@ -0,0 +1,31 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  const isEqual = (currentElement, currentIndex) => currentElement === arrayTwo[currentIndex];
+  return arrayOne.every(isEqual);
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅Assertion Passed: Arrays are equal✅`);
+  } else {
+    console.log(`🛑Assertion Failed: Arrays are not equal🛑`);
+  }
+};
+
+const flatten = function(array) {
+  let newArray = [];
+
+  for (let i in array) {
+    if (array[i] instanceof Array) {
+      for (let j in array[i]) {
+        newArray.push(array[i][j]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+console.log(flatten([1, 2, [3, 4, 10, 45], 5, [6, 4646, 666]]));
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
