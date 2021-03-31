0519577381106da36fe95189a00258005ebfc361
James Brown <brownj95@gmail.com>
2021-03-31 18:45:48 +0000

    implemented letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,34 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  const isEqual = (currentElement, currentIndex) => currentElement === arrayTwo[currentIndex];
+  return arrayOne.every(isEqual);
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅Assertion Passed: Arrays are equal✅`);
+  } else {
+    console.log(`🛑Assertion Failed: Arrays are not equal🛑`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  let pos = 0;
+
+  for (let letter of sentence) {
+    if (letter !== ' ') {
+      if (results[letter]) {
+        results[letter].push(pos);
+      } else {
+        results[letter] = [pos];
+      }
+    }
+    pos++;
+  }
+  return results;
+};
+
+const test = 'hello';
+
+assertArraysEqual(letterPositions(test).h, [0]);
+assertArraysEqual(letterPositions('lighthouse in the house').i, [1, 11]);
