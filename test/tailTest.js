484ce4fc7bf2abd4942aa0c18778720bf167f2a6
James Brown <brownj95@gmail.com>
2021-04-05 16:00:33 +0000

    mocha tests for middle and tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,21 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const words = ['Yo Yo', 'Lighthouse', 'Labs'];
-console.log(tail(words));
-assertEqual(words.length, 3);
-console.log(tail([]));
-console.log(tail([5]));
+
+// console.log(tail([]));
+// console.log(tail([5]));
+
+describe ('#tail', () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns [2] for [1, 2]", () => {
+    assert.deepEqual(tail([1, 2]), [2]);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
