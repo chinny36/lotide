6c486fa9fecb69ed750f5dba160492b28e59a0d6
James Brown <brownj95@gmail.com>
2021-04-05 15:42:01 +0000

    began implementing mocha tests starting with head

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,17 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-console.log(head([1]));
-console.log(head([]));
-
-assertEqual(head([4,5,6]), 4);
-assertEqual(head(["Hello", "Mister", "Anderson"]), "Hello");
-assertEqual(head(["Hello", "Mister", "Anderson"]), "Mister");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("returns 'Hello' for ['Hello', 'Mister', 'Anderson']", () => {
+    assert.strictEqual(head(['Hello', 'Mister', 'Anderson']), 'Hello');
+  });
+});
